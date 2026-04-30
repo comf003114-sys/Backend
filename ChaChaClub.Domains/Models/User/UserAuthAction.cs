3b0f4f2d4e37f5da6f4013867c26da08650eb459
AnonymouS <comf003114@gmail.com>
2026-04-30 09:15:43 +0300

    feat: add user auth DTOs

---
+++ ChaChaClub.Domains/Models/User/UserAuthAction.cs
@@ -0,0 +1,15 @@
+namespace ChaChaClub.Domains.Models.User
+{
+    public class LoginDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class RegisterDto
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
