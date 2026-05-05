d34669686af4836fe9de44df6214d3ff899c26dd
waifeez <cotlecicovrpma@gmail.com>
2026-05-05 21:37:20 +0300

    feat: update BusinessLogic factory with Categories and Dishes

--- ChaChaClub.BusinessLogic/BusinessLogic.cs
+++ ChaChaClub.BusinessLogic/BusinessLogic.cs
@@ -1,4 +1,7 @@
+using AutoMapper;
 using ChaChaClub.BusinessLogic.Functions.Auth;
+using ChaChaClub.BusinessLogic.Functions.Categories;
+using ChaChaClub.BusinessLogic.Functions.Dishes;
 using ChaChaClub.BusinessLogic.Interface;
 using ChaChaClub.DataAccess;
 
@@ -8,13 +11,17 @@ namespace ChaChaClub.BusinessLogic
     {
         private readonly DbSession _session;
         private readonly string _jwtSecret;
+        private readonly IMapper _mapper;
 
-        public BusinessLogic(DbSession session, string jwtSecret)
+        public BusinessLogic(DbSession session, string jwtSecret, IMapper mapper)
         {
             _session = session;
             _jwtSecret = jwtSecret;
+            _mapper = mapper;
         }
 
         public IAuth Auth() => new AuthFlow(_session, _jwtSecret);
+        public ICategories Categories() => new CategoriesFlow(_session, _mapper);
+        public IDishes Dishes() => new DishesFlow(_session, _mapper);
     }
 }
